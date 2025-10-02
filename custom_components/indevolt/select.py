43e5a7e5f2a13921a5fa1ec25ec015ed75d1d047
Erik - Selfmadestrom <info@selfmadestrom.de>
2025-10-02 19:15:55 +0200

    Update select.py

--- custom_components/indevolt/select.py
+++ custom_components/indevolt/select.py
@@ -6,16 +6,13 @@ from . import DOMAIN, IndevoltDataUpdateCoordinator
 
 _LOGGER = logging.getLogger(__name__)
 
-
 async def async_setup_entry(hass, entry, async_add_entities):
-    """Set up Indevolt selects from config entry."""
     coordinator: IndevoltDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
     entities = []
-
     device_map = coordinator.device_map.get("entities", {})
 
     for key, meta in device_map.items():
-        if "enum" in meta:  # Nur Enums
+        if meta.get("writable") and meta.get("enum"):
             entities.append(
                 IndevoltSelect(
                     coordinator,
@@ -26,35 +23,25 @@ async def async_setup_entry(hass, entry, async_add_entities):
                 )
             )
 
-    _LOGGER.debug(
-        "Adding %d Indevolt selects for model %s", len(entities), coordinator.model
-    )
     async_add_entities(entities)
 
-
 class IndevoltSelect(CoordinatorEntity, SelectEntity):
-    """Representation of an Indevolt select."""
-
-    def __init__(self, coordinator, entry_id, key, name, options):
+    def __init__(self, coordinator, entry_id, key, name, enum_map):
         super().__init__(coordinator)
         self._key = str(key)
         self._attr_name = name
         self._attr_unique_id = f"indevolt_{entry_id}_{key}"
-        self._options_map = {int(k): v for k, v in options.items()}  # {1000:"Static"}
-        self._reverse_map = {v: k for k, v in self._options_map.items()}  # {"Static":1000}
-        self._attr_options = list(self._options_map.values())
+        self._enum_map = enum_map
+        self._attr_options = list(enum_map.values())
 
     @property
     def current_option(self):
         raw = self.coordinator.data.get(self._key)
-        return self._options_map.get(raw)
+        return self._enum_map.get(str(raw))
 
     async def async_select_option(self, option: str):
-        """Set a new option on the device."""
-        if option not in self._reverse_map:
-            _LOGGER.error("Invalid option %s for %s", option, self._attr_name)
-            return
-        value = self._reverse_map[option]
-        _LOGGER.debug("Setting %s (%s) to %s", self._attr_name, self._key, option)
-        await self.coordinator.client.async_setdata(self._key, value)
-        await self.coordinator.async_request_refresh()
+        reverse_map = {v: int(k) for k, v in self._enum_map.items()}
+        value = reverse_map.get(option)
+        if value is not None:
+            await self.coordinator.client.async_setdata(int(self._key), [value])
+            await self.coordinator.async_request_refresh()
