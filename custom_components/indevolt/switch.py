ffca6321b72f9be297058c556fbd816c6ec9e02a
Erik - Selfmadestrom <info@selfmadestrom.de>
2025-10-02 17:00:03 +0200

    Update switch.py

--- custom_components/indevolt/switch.py
+++ custom_components/indevolt/switch.py
@@ -1,35 +1,41 @@
-# custom_components/indevolt/switch.py
+import logging
 from homeassistant.components.switch import SwitchEntity
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
+
 from . import DOMAIN
 
+_LOGGER = logging.getLogger(__name__)
+
+async def async_setup_entry(hass, entry, async_add_entities):
+    data = hass.data[DOMAIN][entry.entry_id]
+    coordinator = data["coordinator"]
+    client = data["client"]
+    device_map = data["device_map"]
+
+    entities = []
+    for e in device_map.get("entities", []):
+        if e.get("platform") == "switch":
+            entities.append(IndevoltSwitch(coordinator, client, entry.entry_id, e))
+
+    async_add_entities(entities)
+
 class IndevoltSwitch(CoordinatorEntity, SwitchEntity):
-    def __init__(self, coordinator, client, name, key, state_value):
+    def __init__(self, coordinator, client, entry_id, definition: dict):
         super().__init__(coordinator)
-        self.client = client
-        self._attr_name = name
-        self._key = str(key)
-        self._state_value = state_value
+        self._client = client
+        self._entry_id = entry_id
+        self._key = str(definition["t"])
+        self._attr_name = definition.get("name", f"Switch {self._key}")
+        self._attr_unique_id = f"indevolt_{entry_id}_{self._key}"
 
     @property
     def is_on(self):
-        return self.coordinator.data.get(self._key) == self._state_value
+        return bool(self.coordinator.data.get(self._key, 0))
 
     async def async_turn_on(self, **kwargs):
-        await self.client.async_setdata(47015, [self._state_value, 0, 0])
+        await self._client.async_setdata(int(self._key), [1])
         await self.coordinator.async_request_refresh()
 
     async def async_turn_off(self, **kwargs):
-        await self.client.async_setdata(47015, [0, 0, 0])
+        await self._client.async_setdata(int(self._key), [0])
         await self.coordinator.async_request_refresh()
-
-async def async_setup_entry(hass, entry, async_add_entities):
-    inst = hass.data[DOMAIN][entry.entry_id]
-    coord = inst["coordinator"]
-    client = inst["client"]
-
-    entities = [
-        IndevoltSwitch(coord, client, "Force Charge", 47015, 1),
-        IndevoltSwitch(coord, client, "Force Discharge", 47015, 2),
-    ]
-    async_add_entities(entities, True)
